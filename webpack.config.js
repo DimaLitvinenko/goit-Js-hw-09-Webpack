014505f3738a2ea2590b720521473bc8b859ca79
DimaLitvinenko <dimaly.too@gmail.com>
2022-02-05 23:12:46 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
@@ -8,22 +8,20 @@ module.exports = {
         main: path.resolve(__dirname, './src/index.js'), // точка входа
     },
     output: {
-        path: path.resolve(__dirname, './dist'),         // точка выхода
-        filename: 'main.js',                    
+        path: path.resolve(__dirname, './dist'), // точка выхода
+        filename: 'main.js',
     },
     module: {
         rules: [
             {
                 test: /\.s[ac]ss$/i,
-                use: 
-                [
+                use: [
                     //  'style-loader',        // Создает узлы `style` из строк JS
-                    MiniCssExtractPlugin.loader, 
-                    'css-loader',           // Переводит CSS в CommonJS 
-                    'sass-loader'           // Компилирует Sass в CSS
-                ],                  
+                    MiniCssExtractPlugin.loader,
+                    'css-loader', // Переводит CSS в CommonJS
+                    'sass-loader', // Компилирует Sass в CSS
+                ],
             },
-            { test: /\.handlebars$/, loader: "handlebars-loader" },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -33,7 +31,7 @@ module.exports = {
                 test: /\.(png|jpg|gif|svg)$/i,
                 type: 'asset/resource',
                 generator: {
-                  filename: 'images/[name][ext]',
+                    filename: 'images/[name][ext]',
                 },
             },
         ],
@@ -41,15 +39,14 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Webpack Boilerplate',
-            template: path.resolve(__dirname, './src/index.html'),  // шаблон
-            filename: 'index.html',                                 // название выходного файла
+            template: path.resolve(__dirname, './src/index.html'), // шаблон
+            filename: 'index.html', // название выходного файла
         }),
         new MiniCssExtractPlugin({ filename: 'styles.css' }),
         new CleanWebpackPlugin(),
-
     ],
     devServer: {
         port: 4444,
-        open: true,  
+        open: true,
     },
-}
+};
